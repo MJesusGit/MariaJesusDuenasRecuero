0c85a2cefdb8375baae87056da012ba5587d54a8
María Jesús Dueñas Recuero <72667996+MJesusGit@users.noreply.github.com>
2022-11-18 14:20:02 +0100

    github pages

---
+++ script.js
@@ -0,0 +1,55 @@
+/*Animacion menu responsive*/
+
+addEventListener('DOMContentLoaded',() => {
+    const btn_menu = document.querySelector('.btn_menu')
+    if(btn_menu){
+        btn_menu.addEventListener('click', () => {
+            const menu_items = document.querySelector('.menu_items')
+            menu_items.classList.toggle('show')
+        })
+
+    }
+})
+
+//Función que me aplica el estilo a la opciòn seleccionada y quita la previamente seleccionada
+function seleccionar(links) {
+    var opciones = document.querySelectorAll('#links  a');
+    opciones[0].className = "";
+    opciones[1].className = "";
+    opciones[2].className = "";
+    opciones[3].className = "";
+    opciones[4].className = "";
+    links.className = "seleccionado";
+}
+
+//apartado de tabs-formacion
+
+/*==================== QUALIFICATION TABS ====================*/
+const tabs = document.querySelectorAll("[data-target]"),
+    tabContents = document.querySelectorAll("[data-content]")
+
+tabs.forEach(tab =>{
+    tab.addEventListener("click", () =>{
+        const target = document.querySelector(tab.dataset.target)
+        tabContents.forEach(tabContent => {
+            tabContent.classList.remove("qualification__active")
+        })
+        target.classList.add("qualification__active")
+
+        tabs.forEach(tab =>{
+            tab.classList.remove("qualification__active")
+        })
+        tab.classList.add("qualification__active")
+    })
+})
+
+//progress bar
+CSS.registerProperty({
+    name: "--p",
+    syntax: "<integer>",
+    initialValue: 0,
+    finalValue:40,
+    inherits: true,
+  });
+
+
