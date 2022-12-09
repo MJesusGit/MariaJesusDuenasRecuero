329db360ae81567eabaa2c7fdcec23706ab90f40
MJ <MJesus.duenas1@alu.uclm.es>
2022-12-09 02:11:13 +0100

    version5.0

--- script.js
+++ script.js
@@ -1,26 +1,26 @@
 
-//Función que me aplica el estilo a la opciòn seleccionada y quita la previamente seleccionada
-function seleccionar(links) {
-    var opciones = document.querySelectorAll('#links a');
-    opciones[0].className = "";
-    opciones[1].className = "";
-    opciones[2].className = "";
-    opciones[3].className = "";
-    opciones[4].className = "";
-    links.className = "seleccionado";
-}
+// //Función que me aplica el estilo a la opciòn seleccionada y quita la previamente seleccionada
+// function seleccionar(links) {
+//     var opciones = document.querySelectorAll('#links a');
+//     opciones[0].className = "";
+//     opciones[1].className = "";
+//     opciones[2].className = "";
+//     opciones[3].className = "";
+//     opciones[4].className = "";
+//     links.className = "seleccionado";
+// }
 
-/*Animacion menu responsive*/
- addEventListener('DOMContentLoaded',() => {
-     const btn_menu = document.querySelector('.btn_menu')
-    if(btn_menu){
-         btn_menu.addEventListener('click', () => {
-             const menu_items = document.querySelector('.menu_items')
-             menu_items.classList.toggle('show')
-         })
+// /*Animacion menu responsive*/
+//  addEventListener('DOMContentLoaded',() => {
+//      const btn_menu = document.querySelector('.btn_menu')
+//     if(btn_menu){
+//          btn_menu.addEventListener('click', () => {
+//              const menu_items = document.querySelector('.menu_items')
+//              menu_items.classList.toggle('show')
+//          })
 
-    }
- })
+//     }
+//  })
 
 
 
@@ -28,7 +28,6 @@ function seleccionar(links) {
 
 
 //apartado de tabs-formacion
-/*==================== QUALIFICATION TABS ====================*/
 const tabs = document.querySelectorAll("[data-target]"),
     tabContents = document.querySelectorAll("[data-content]")
 
@@ -46,6 +45,7 @@ tabs.forEach(tab =>{
         tab.classList.add("qualification__active")
     })
 })
+
 
 
 
